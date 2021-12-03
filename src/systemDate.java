42df36174f586660790d70d70a0237818d4a6c7b
Vương Nguyễn <vuongnqv@gmail.com>
2021-12-03 11:32:33 +0700

    thêm file

---
+++ src/systemDate.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+import java.util.Scanner;
+
+public class systemDate {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
